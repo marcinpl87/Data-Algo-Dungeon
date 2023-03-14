daa8fed9df82fac7aa1c551d3aaf80c9a5a14a30
marcinpl87 <poczta.maciejewski@gmail.com>
2023-03-14 22:02:30 +0100

    Sidebar and layout from library integrated into the app

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,56 @@
 import React from 'react';
+import {
+    Routes,
+    Route,
+    Outlet,
+    NavLink,
+} from "react-router-dom";
+import {
+    FaBook,
+    FaHome,
+} from 'react-icons/fa';
+import {
+    Layout,
+    Sidebar,
+} from 'marcinpl87-library';
 import Home from './pages/home';
 
+const getNavLink = (url) => {
+    return ({children}) => (
+        <NavLink
+            className={({isActive}) =>
+                isActive
+                    ? "active"
+                    : ""
+            }
+            to={url}
+        >
+            {children}
+        </NavLink>
+    );
+};
+
 export default () => (
-    <Home />
+    <Layout
+        sidebar={Sidebar}
+        menu={[
+            {
+                anchor: "Home",
+                iconComp: FaHome,
+                linkComp: getNavLink("/"),
+            },
+            {
+                anchor: "Subpage",
+                iconComp: FaBook,
+                linkComp: getNavLink("/test-url2"),
+            },
+        ]}
+    >
+        <Routes>
+            <Route path="/" element={<Outlet />}>
+                <Route index element={<Home />} />
+                <Route path="test-url2" element={<div>Sub Page</div>} />
+            </Route>
+        </Routes>
+    </Layout>
 );
